2a0ea2e26750c3468adb075f49f6e6337ee9fcf5
accomplishlee <accomplishlee@hanwha.com>
2020-12-09 16:42:27 +0900

    Add Refresh Scope

--- post/src/main/java/example/scg/post/PostController.java
+++ post/src/main/java/example/scg/post/PostController.java
@@ -2,6 +2,7 @@ package example.scg.post;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/posts")
 @RequiredArgsConstructor
+@RefreshScope
 public class PostController {
 
     @Value("${server.port}")
